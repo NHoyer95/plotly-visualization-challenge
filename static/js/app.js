76bdef94250caa06464c4622d791082f26af065b
NHoyer95 <hoyer047@umn.edu>
2021-04-27 17:07:35 -0500

    Created bargraph

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,40 @@
 
 console.log("app.js loaded");
 
+// Define function for creating the graphs
 function DrawBargraph(sampleId) {
     console.log(`DrawBargraph(${sampleId})`);
 
+    d3.json("data/samples.json").then(data => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+    })
 }
 
 function DrawBubblechart(sampleId) {
