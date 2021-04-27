f660ad7f7a740819df1f8b0b302ce423083a704b
NHoyer95 <hoyer047@umn.edu>
2021-04-27 16:37:15 -0500

    Added event handler and stubs for functions

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,31 @@
+// Starter code in this file is based on Dom's office hours demo for hw 15
+
 console.log("app.js loaded");
 
-// Starter code in this file is based on Dom's office hours demo for hw 15
+function DrawBargraph(sampleId) {
+    console.log(`DrawBargraph(${sampleId})`);
+
+}
+
+function DrawBubblechart(sampleId) {
+    console.log(`DrawBubblechart(${sampleId})`);
+
+}
+
+function DisplayMetadata(sampleId) {
+    console.log(`DisplayMetadata(${sampleId})`);
+
+}
+
+function optionChanged(newSampleId) {
+    console.log(`User selected ${newSampleId}`);
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    DisplayMetadata(newSampleId);
+}
+
+
 function InitDashboard() {
     
     console.log("InitDashboard()");
@@ -10,7 +35,7 @@ function InitDashboard() {
 
     d3.json("data/samples.json").then(data => {
         
-        console.log(data);
+        // console.log(data);
     
         var sampleNames = data.names;
 
@@ -20,7 +45,18 @@ function InitDashboard() {
             .text(sampleId)
             .property("value", sampleId);
         });
+
+        var id = sampleNames[0];
+
+        DrawBargraph(id);
+
+        DrawBubblechart(id);
+
+        DisplayMetadata(id);
+
+
     });
+
     // Update the bargraph
 
     // Update the bubblechart
